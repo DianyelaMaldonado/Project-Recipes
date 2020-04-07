7d9dd85f0768f00b898c5f127bb9d93e0b9f211a
Dianyela Maldonado <dianyela.y.maldonado@gmail.com>
2020-04-07 16:33:06 -0500

    añadí cambios en carpetas index data style

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     var sliderVisibility = document.querySelector('#sliderView');
-    loadJSON();
-    loadJSONTwo();
-    loadJSONThree();
+    buildHome();
+
+
 });
 
 
-function loadJSON() {
+function loadJSONOne() {
     fetch('http://api.imagendigital.com/v2/cocinadelirante/node.json/339fd1e0444ddbbd4d4528d8df161108?fields=id,title,url,taxonomy.name,created,image,author.name&type=receta&limit=6')
         .then(res => {
             return res.json()
@@ -79,15 +79,13 @@ function loadJSONThree() {
         .then(res => {
             return res.json()
         })
-        .then(function(result_json2) {
+        .then(function(result_json3) {
             noteVisibility = document.querySelector('#noteView');
-            // console.log(noteVisibility);
             let notes = '';
-            let notesRecipes = result_json2.data;
-            console.log(notesRecipes[0]);
+            let notesRecipes = result_json3.data;
+            // console.log(notesRecipes[0]);
             for (var noteRecipesNum in notesRecipes) {
                 let data_note = notesRecipes[noteRecipesNum];
-                console.log(noteRecipesNum);
                 var encounterDate = data_note['created'];
                 var formattedDate = timestamp(encounterDate);
 
@@ -129,11 +127,87 @@ function loadJSONThree() {
                     </div>
                     `
                 }
-
-
             }
             noteVisibility.innerHTML = notes;
-
         });
 
+}
+
+function loadJSONFour() {
+    fetch('http://api.imagendigital.com/v2/cocinadelirante/node.json/339fd1e0444ddbbd4d4528d8df161108?fields=id,title,summary,url,taxonomy.name,created,image.styles.gallerie,author.name&type=receta&limit=3&offset=8')
+        .then(res => {
+            return res.json()
+        })
+        .then(function(result_json4) {
+            someCardsVisibility = document.querySelector('#contentleft');
+            let someCards = '';
+            let visibilyCards = result_json4.data;
+            console.log(visibilyCards);
+
+            for (var visibilyCardNum in visibilyCards) {
+                let data_cardVisibility = visibilyCards[visibilyCardNum];
+                var encounterDate = data_cardVisibility['created'];
+                var formattedDate = timestamp(encounterDate);
+                someCards += `   <div class="row">
+                            <div class="col s6">
+                                 <img class="responsive-img" src="${data_cardVisibility['image']['styles']['gallerie']}">
+                            </div>
+                            <div class="col s6">
+                                <div class="textRecipeInformation">
+                                    <h6 class="subtitlefour">${data_cardVisibility['taxonomy']['name']}</h6>
+                                    <h4 class="titleRecipefour">${data_cardVisibility['title']}</h4>
+                                    <h6 class="dateAndAutorbigbox">${formattedDate}/ ${data_cardVisibility['author']['name']}</h6>
+                                    <p class="recipeNoteDescriptionfour">${data_cardVisibility['summary']}</p>
+                                </div>
+                            </div>
+                        </div> 
+                                   
+                `;
+            }
+            someCardsVisibility.innerHTML = someCards;
+        })
+}
+
+function loadJSONFive() {
+    fetch('http://api.imagendigital.com/v2/cocinadelirante/node.json/339fd1e0444ddbbd4d4528d8df161108?fields=id,title,summary,url,taxonomy.name,created,image.styles.gallerie,author.name&type=receta&limit=5&offset=11')
+        .then(res => {
+            return res.json()
+        })
+        .then(function(result_json5) {
+            littleCardsVisibility = document.querySelector('#contentRight');
+            let LittlesomeCards = '';
+            let visibilyLittleCards = result_json5.data;
+            console.log(visibilyLittleCards);
+
+            for (var visibilyLittleCardNum in visibilyLittleCards) {
+                let data_littleCards = visibilyLittleCards[visibilyLittleCardNum];
+                console.log(visibilyLittleCardNum);
+                var encounterDate = data_littleCards['created'];
+                var formattedDate = timestamp(encounterDate);
+
+                LittlesomeCards += ` 
+                                    <li>
+                                        <div class="row">
+                                            <div class="col s4">
+                                               <img class="responsive-img" src="${data_littleCards['image']['styles']['gallerie']}"> 
+                                            </div>
+
+                                            <div class="col s8">
+                                                <h4 class="titlelittlebox">${data_littleCards['title']}</h4>
+                                                <h6 class="dateAndAutorlittlebox">${formattedDate}/ ${data_littleCards['author']['name']}</h6>
+                                             </div>
+                                        </div>
+                                    </li>  
+                 `;
+            }
+            littleCardsVisibility.innerHTML = LittlesomeCards;
+        })
+}
+
+function buildHome() {
+    loadJSONOne();
+    loadJSONTwo();
+    loadJSONThree();
+    loadJSONFour();
+    loadJSONFive();
 }
